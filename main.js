dbeafa2c1e00836e0cb027f3d7f64c5e227b1bfe
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-07-22 15:43:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,34 @@ let b = new Promise((resolve, reject) => {
 b.then(() => {
     console.log(a)
 })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const obj = {
+    name: 'Vanya',
+    age: 30,
+};
+
+const newPeople = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(obj)
+        reject('error')
+    },)
+});
+
+// newPeople().then((oke) => console.log(oke))
+// newPeople().catch((ok) => console.error(ok))
+
+
+
+const newJeckt = async () => {
+    try {
+        const b = await newPeople();
+        console.log(b)
+    } catch (err) {
+        console.error(err)
+    }finally {
+        console.log(1)
+    }
+};
+
+newJeckt()
